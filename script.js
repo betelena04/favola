d04080a0fe902647e6536adeb37c59527f7b831d
Cristian-creator <cristian_upt@yahoo.com>
2023-04-30 16:34:13 +0300

    updated css,img and js path

--- script.js
+++ script.js
@@ -45,25 +45,7 @@ console.log('a');
 
     $('.owl-carousel').owlCarousel({
         loop: true,
-        // nav: true,
         items: 1,
         autoplay: true,
         autoplayTimeout: 6000
-        // autoplayHoverPause: true,
-        // animateOut: 'fadeOut',
-        // center: true,
-        // margin: 12,
-        // responsive: {
-        //     0: {
-        //         nav: false
-        //     },
-
-        //     575: {
-        //         margin: 10
-        //     },
-
-        //     1200: {
-        //         nav: true
-        //     }
-        // }
     });
